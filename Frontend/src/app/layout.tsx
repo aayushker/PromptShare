60982e017c14a1f6f585bc2b49ad4047391c4334
aayushker <singhaayushker@gmail.com>
2024-06-08 19:45:29 +0530

    Removed buffer code and components

--- Frontend/src/app/layout.tsx
+++ Frontend/src/app/layout.tsx
@@ -1,13 +1,10 @@
 import '../styles/globals.css'
-import Nav from '../components/Nav'
-import Provider from '../components/Provider'
-
 export const metadata = {
   title: 'PromptShare',
   discription: 'PromptShare is a platform for sharing prompts for writing, drawing, and other creative activities.'
 }
 
-const Root = ({ children }) => {
+const Root = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang='en'> 
       <body>
@@ -16,7 +13,6 @@ const Root = ({ children }) => {
         </div>
 
         <div className="app">
-          <Nav />
           {children}
         </div>
       </body>
